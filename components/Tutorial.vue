667ce58b54f0ac558352a093ee4bff07789c7c13
maotterson <otterson.2@gmail.com>
2021-08-03 14:37:20 -0400

    test form

--- components/Tutorial.vue
+++ components/Tutorial.vue
@@ -10,12 +10,14 @@
         <h2 class="text-2xl leading-7 font-semibold">
           Welcome to your Nuxt Application
         </h2>
-        <p class="mt-3 text-gray-600">
-          We recommend you take a look at the <a href="https://nuxtjs.org" target="_blank" class="text-green-500 hover:underline">Nuxt documentation</a>, whether you are new or have previous experience with the framework.<br>
-        </p>
-        <p class="mt-4 pt-4 text-gray-800 border-t border-dashed">
-          To get started, remove <code class="bg-gray-100 text-sm p-1 rounded border">components/Tutorial.vue</code> and start coding in <code class="bg-gray-100 text-sm p-1 rounded border">pages/index.vue</code>. Have fun!
-        </p>
+        
+        <form class="p-8 flex flex-col" name="contact" method="POST" data-netlify="true">
+          <div class="p-8">
+            <label for="text-name">Name</label>
+            <input class="border" type="text" name="name" id="text-name">
+          </div>
+          <button class="border" type="submit">Submit</button>
+        </form>
       </div>
       <div class="flex justify-center pt-4 space-x-2">
         <a href="https://github.com/nuxt/nuxt.js" target="_blank"><svg
